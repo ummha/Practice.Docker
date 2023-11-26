7054eeb8a192af9a8d7b05d570a29ef60946aa5e
MinseoRhie <dlalstj0213@gmail.com>
2023-11-27 02:35:58 +0900

    project(docker): redis 세션 공유 설정

--- api-delivery/src/main/java/com/practice/delivery/DeliveryApplication.java
+++ api-delivery/src/main/java/com/practice/delivery/DeliveryApplication.java
@@ -2,7 +2,9 @@ package com.practice.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
+@EnableRedisHttpSession
 @SpringBootApplication
 public class DeliveryApplication {
     public static void main(String[] args) {
--- api-market/src/main/java/com/practice/market/controller/MarketController.java
+++ api-market/src/main/java/com/practice/market/controller/MarketController.java
@@ -1,10 +1,17 @@
 package com.practice.market.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -16,5 +23,40 @@ public class MarketController {
     String market() {
         log.info("REQUEST :: request:url:{}", servletRequest.getRequestURL());
         return "This is my market!";
+    }
+
+    @GetMapping("/session")
+    ResponseEntity<Map<String, Object>> session() {
+        HttpSession httpSession = servletRequest.getSession();
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("id", httpSession.getId());
+        body.put("createdBy", httpSession.getAttribute("createdBy"));
+        body.put("modifiedBy", httpSession.getAttribute("modifiedBy"));
+        return ResponseEntity.ok(body);
+    }
+
+    @PutMapping("/session")
+    ResponseEntity<Map<String, Object>> updateSession() {
+        HttpSession httpSession = servletRequest.getSession();
+        if(httpSession.getAttribute("createdBy") == null) {
+            httpSession.setAttribute("createdBy", "market");
+        }
+        httpSession.setAttribute("modifiedBy", "market");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("id", httpSession.getId());
+        return ResponseEntity.ok(body);
+    }
+
+    @PostMapping("/session")
+    ResponseEntity<Map<String, Object>> createSession() {
+        HttpSession httpSession = servletRequest.getSession();
+        httpSession.setAttribute("createdBy", "market");
+        httpSession.setAttribute("modifiedBy", "market");
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("id", httpSession.getId());
+        return ResponseEntity.ok(body);
     }
 }
