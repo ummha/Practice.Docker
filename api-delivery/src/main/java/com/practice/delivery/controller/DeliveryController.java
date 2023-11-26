1bd15c4125b1ca6fa3d66f6611f61bb3a17dec09
MinseoRhie <dlalstj0213@gmail.com>
2023-11-26 23:03:48 +0900

    project(docker): 세부 설정 변경
    build.gradle - dependsOn 태스크 의존성 추가
    docker-compose.yml - cleanBuilder 서비스 제거
    nginx.conf - 프록시 방식 변경
    application.yml - 프로퍼티 파일 추가

--- api-delivery/src/main/java/com/practice/delivery/controller/DeliveryController.java
+++ api-delivery/src/main/java/com/practice/delivery/controller/DeliveryController.java
@@ -1,13 +1,20 @@
 package com.practice.delivery.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequiredArgsConstructor
 public class DeliveryController {
+    private final HttpServletRequest servletRequest;
 
-    @GetMapping("/delivery")
+    @GetMapping("")
     String delivery() {
+        log.info("REQUEST :: request:url:{}", servletRequest.getRequestURL());
         return "Deliver Soon!";
     }
 }
